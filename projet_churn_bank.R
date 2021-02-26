494a377a2098a907c1f1183ca20beefaf39ae260
Rod des Grenettes <rodrigue.bernard@gmail.com>
2021-02-26 14:03:14 +0100

    Update projet_churn_bank.R

--- projet_churn_bank.R
+++ projet_churn_bank.R
@@ -35,65 +35,30 @@ if("leaps" %in% rownames(installed.packages()) == FALSE) {install.packages("leap
 ##################################################################################
 # 2 - Import des données 
 ##################################################################################
-# setwd(".")
+
 # (10127 matchs)
 data <- read.csv("data/BankChurners.csv", sep = ",")
 
 # Vérification des types de champs et des valeurs nulles
 summary(data)
-summary(data$Attrition_Flag)
+str(data)
 
-data_clean <- data[,-c(1,22,23)] # On supprime les 2 dernières colonnes inutiles
+# Retrait des colonnes 
+# - CLIENTNUM
+# - Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1
+# - Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2
+data <- data[,-c(1,22,23)]
 
+# Séparation des clients qui ont quitté la banque de ceux qui sont resté
 data$Attrition_Flag<-as.character(data$Attrition_Flag)
 data$Attrition_Flag[data$Attrition_Flag=="Existing Customer"]<-"Stay"
 data$Attrition_Flag[data$Attrition_Flag=="Attrited Customer"]<-"Quit"
-data_quit<-data[(data$Attrition_Flag)=="Quit",]
-data_quit %>% select(where(is.numeric)) %>% as.matrix() %>%
-    cor() %>% corrplot(method = "number", type="lower")
+data_quit <- data[(data$Attrition_Flag)=="Quit",]
 
+# Analyse de chaque variable de manière séparée
+categ <- data_quit %>%
+    select(Attrition_Flag,Gender, Marital_Status, Card_Category, Income_Category, Education_Level)
 
-#plot("Attrition_Flag", "Customer_Age", data = data)
-
-hist(data_clean$Total_Trans_Amt)
-
-
-
-# mutates
-data_corr <- data_clean %>% mutate_if(is.factor,as.numeric)
-data_colnames <- colnames(data_clean)
-data_colnames
-# compute correlation 
-correlation= cor(data_corr)
-# correlation as data.frame
-target_corr= as.data.frame(correlation[,1])
-rownames(target_corr)<-data_colnames
-# correlation column name
-colnames(target_corr) <-'Correlation'
-# sort dataframe
-target_corr <- target_corr %>% arrange(desc(Correlation))
-# exclude target
-target_corr <- target_corr %>% filter(Correlation<1) 
-# round
-target_corr <- round(target_corr,2)
-
-data_clean %>%
-    select(where(is.numeric)) %>%
-    as.matrix() %>%
-    cor() %>%
-    corrplot(method = "number", type="lower")
-
-# PLOT CORRELATION
-target_corr %>% arrange(desc(Correlation)) %>%
-    ggplot(aes(x=Correlation,
-               y=reorder(rownames(target_corr), Correlation),
-               fill=Correlation)) +
-    geom_col(color='black') + labs(title='Target Correlation', y='Variables') +
-    theme_classic() +
-    theme(legend.position = 'none')
-
-#Plot par genre
-categ <- data_clean %>% select(Attrition_Flag,Gender, Marital_Status, Card_Category, Income_Category, Education_Level)
 categ %>%
     select(Attrition_Flag,Gender) %>%
     mutate(Gender = ifelse(Gender == "F", "Female", "Male")) %>%
@@ -107,6 +72,13 @@ categ %>%
               position = position_dodge(.9))+
     labs(title="Distribution par genre",
          x="Attrition_Flag",y="NB")
+
+# Graphique des corrélations entre chacune des variables
+data_quit %>%
+    select(where(is.numeric)) %>%
+    as.matrix() %>%
+    cor() %>%
+    corrplot(method = "number", type="lower")
 
 # Obtaining the correlation matrix with the Spearman method
 cor_spearman <- cor(dados_cor[, sapply(dados_cor, is.numeric)], method = 'spearman')
@@ -132,8 +104,6 @@ p2 <- data_clean %>% ggplot(aes(x=Total_Trans_Ct, fill=Attrition_Flag)) +
     labs(title='Total Transaction Count (Last 12 months) by flag') +
     theme(legend.position='bottom')
 
-# supprimer IGNORER LES 2 DERNIÈRES COLONNES (NAIVE BAYES CLAS…)
-
 
 # variable dependante Y = départ ou non : attrition_flag
 # variable qualitative donc on fera une régression logistique
@@ -154,45 +124,72 @@ p2 <- data_clean %>% ggplot(aes(x=Total_Trans_Ct, fill=Attrition_Flag)) +
 # age, genre, niveau education, statut marital, personnes à charge, revenu, type de carte, période de relation avec la banque...
 
 
-model_quali<-glm(Attrition_Flag~Customer_Age, data=data, family= binomial(logit))
+# model_quali<-glm(Attrition_Flag~Customer_Age, data=data, family= binomial(logit))
+# 
+# # Interprétation
+# model_quali
+# summary(model_quali)
+# exp(coef(model_quali))
+# 
+# # Matrice de confusion
+# appren.p <- cbind(data_reg, predict(model_quali, newdata = data_reg, type = "link", 
+#                                     se = TRUE))
+# appren.p <- within(appren.p, {
+#     PredictedProb <- plogis(fit)
+#     LL <- plogis(fit - (1.96 * se.fit))
+#     UL <- plogis(fit + (1.96 * se.fit))
+# })
+# appren.p <- cbind(appren.p, pred.chd = factor(ifelse(appren.p$PredictedProb > 0.5, 1, 0)))
+# colnames(appren.p)
+# appren.p<-appren.p[,c("Winner","diff_dist_att","diff_dist_att_class","fit","PredictedProb","pred.chd")]
+# (m.confusion <- as.matrix(table(appren.p$pred.chd, appren.p$Winner)))
+# 
+# # Taux de bien classé
+# (m.confusion[1,1]+m.confusion[2,2]) / sum(m.confusion)
+# 
+# # Sensibilité
+# (m.confusion[2,2]) / (m.confusion[2,2]+m.confusion[1,2])
+# 
+# # Sensibilité
+# (m.confusion[2,2]) / (m.confusion[2,2]+m.confusion[1,2])
+# 
+# # Spécificité 
+# (m.confusion[1,1]) / (m.confusion[1,1]+m.confusion[2,1])
+# 
+# 
+# # ODs ratio
+# exp(cbind(coef(model_quali), confint(model_quali)))
+# library(questionr)
+# odds.ratio(model_quali)
+# install.packages("GGally")
+# library(GGally)
+# library(broom.helpers)
+# ggcoef_model(model_quali, exponentiate = TRUE)
 
-# Interprétation
-model_quali
-summary(model_quali)
-exp(coef(model_quali))
 
-# Matrice de confusion
-appren.p <- cbind(data_reg, predict(model_quali, newdata = data_reg, type = "link", 
-                                    se = TRUE))
-appren.p <- within(appren.p, {
-    PredictedProb <- plogis(fit)
-    LL <- plogis(fit - (1.96 * se.fit))
-    UL <- plogis(fit + (1.96 * se.fit))
-})
-appren.p <- cbind(appren.p, pred.chd = factor(ifelse(appren.p$PredictedProb > 0.5, 1, 0)))
-colnames(appren.p)
-appren.p<-appren.p[,c("Winner","diff_dist_att","diff_dist_att_class","fit","PredictedProb","pred.chd")]
-(m.confusion <- as.matrix(table(appren.p$pred.chd, appren.p$Winner)))
-
-# Taux de bien classé
-(m.confusion[1,1]+m.confusion[2,2]) / sum(m.confusion)
-
-# Sensibilité
-(m.confusion[2,2]) / (m.confusion[2,2]+m.confusion[1,2])
-
-# Sensibilité
-(m.confusion[2,2]) / (m.confusion[2,2]+m.confusion[1,2])
-
-# Spécificité 
-(m.confusion[1,1]) / (m.confusion[1,1]+m.confusion[2,1])
-
-
-# ODs ratio
-exp(cbind(coef(model_quali), confint(model_quali)))
-library(questionr)
-odds.ratio(model_quali)
-install.packages("GGally")
-library(GGally)
-library(broom.helpers)
-ggcoef_model(model_quali, exponentiate = TRUE)
+# Graphique des corrélations entre chacune des variables
+# mutates
+# data_corr <- data_clean %>% mutate_if(is.factor, as.numeric)
+# data_colnames <- colnames(data_clean)
+# data_colnames
+# # compute correlation 
+# correlation= cor(data_corr)
+# # correlation as data.frame
+# target_corr= as.data.frame(correlation[,1])
+# rownames(target_corr)<-data_colnames
+# # correlation column name
+# colnames(target_corr) <-'Correlation'
+# # sort dataframe
+# target_corr <- target_corr %>% arrange(desc(Correlation))
+# # exclude target
+# target_corr <- target_corr %>% filter(Correlation<1) 
+# # round
+# target_corr <- round(target_corr,2)
+# target_corr %>% arrange(desc(Correlation)) %>%
+#     ggplot(aes(x=Correlation,
+#                y=reorder(rownames(target_corr), Correlation),
+#                fill=Correlation)) +
+#     geom_col(color='black') + labs(title='Target Correlation', y='Variables') +
+#     theme_classic() +
+#     theme(legend.position = 'none')
 
