0f77dd254ea0ba4251b4d13c7ff7c624719ac8c5
Christine Fouque <christinefouque80@gmail.com>
2021-03-28 12:42:50 +0200

    dependencies=TRUE
    pour avoir les dependances necessaires au niveau des librairies et eviter des packages manquants qui bloquent

--- script/projet_churn_bank.R
+++ script/projet_churn_bank.R
@@ -10,53 +10,53 @@
 # ```{r}
 # data.frame amélioré
 if ("data.table" %in% rownames(installed.packages()) == FALSE) {
-install.packages("data.table")
+install.packages("data.table", dependencies=TRUE)
 }
 library(data.table)
 # gestion des dates
 if ("lubridate" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("lubridate")
+    install.packages("lubridate", dependencies=TRUE)
 }
 library(lubridate)
 # R Companion to Applied Regression (Functions to Accompany J. Fox and S. Weisberg, AThird Edition, Sage, 2019.)
 if ("car" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("car")
+    install.packages("car", dependencies=TRUE)
 }
 library(car)
 # Several functions are available for calculating the most widely used effect sizes (ES),
 # along with their variances, confidence intervals and p-values
 if ("compute.es" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("compute.es")
+    install.packages("compute.es", dependencies=TRUE)
 }
 library(compute.es)
 # Graphical and tabular effect displays, e.g., of interactions, for various statistical models with linear predictors.
 if ("effects" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("effects")
+    install.packages("effects", dependencies=TRUE)
 }
 library(effects)
 # Create Elegant Data Visualisations Using the Grammar of Graphics
 if ("ggplot2" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("ggplot2")
+    install.packages("ggplot2", dependencies=TRUE)
 }
 library(ggplot2)
 # Simultaneous Inference in General Parametric Models
 if ("multcomp" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("multcomp")
+    install.packages("multcomp" ,dependencies=TRUE)
 }
 library(multcomp)
 # Package for Analysis of Space-Time Ecological Series
 if ("pastecs" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("pastecs")
+    install.packages("pastecs", dependencies=TRUE)
 }
 library(pastecs)
 # A Collection of Robust Statistical Methods
 if ("WRS2" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("WRS2")
+    install.packages("WRS2", dependencies=TRUE)
 }
 library(WRS2)
 # set of packages that work in harmony because they share common data representations and 'API' design.
 if ("tidyverse" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("tidyverse")
+    install.packages("tidyverse", dependencies=TRUE)
 }
 library(tidyverse)
 # # if ("dplyr" %in% rownames(installed.packages()) == FALSE) {
@@ -65,41 +65,43 @@ library(tidyverse)
 library(dplyr)
 # Regression subset selection, including exhaustive search.
 if ("leaps" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("leaps")
+    install.packages("leaps", dependencies=TRUE)
 }
 library(leaps)
 # Graphes de corrélation
 if ("corrplot" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("corrplot")
+    install.packages("corrplot",dependencies=TRUE)
 }
 library(corrplot)
 # lib de graphes avancés
 if ("highcharter" %in% rownames(installed.packages()) == FALSE) {
-    install.packages("highcharter")
+    install.packages("highcharter",dependencies=TRUE)
 }
 library(highcharter)
 
 # Tools for reordering and modifying factor levels  with Categorical Variables (Factors)
-if("forcats" %in% rownames(installed.packages()) == FALSE) {install.packages("forcats")};library(forcats)
+if("forcats" %in% rownames(installed.packages()) == FALSE) {install.packages("forcats",dependencies=TRUE)};library(forcats)
 # 
-if("skimr" %in% rownames(installed.packages()) == FALSE) {install.packages("skimr")};library(skimr)
+if("skimr" %in% rownames(installed.packages()) == FALSE) {install.packages("skimr",dependencies=TRUE)};library(skimr)
 # stats
 
 # créer des graphiques prêts à être publiés
-if("ggpubr" %in% rownames(installed.packages()) == FALSE) {install.packages("ggpubr")};library(ggpubr)
+if("ggpubr" %in% rownames(installed.packages()) == FALSE) {install.packages("ggpubr",dependencies=TRUE)};library(ggpubr)
 # Extraire et visualiser les résultats d’analyses de données multivariées
-if("factoextra" %in% rownames(installed.packages()) == FALSE) {install.packages("factoextra")};library(factoextra)
+if("factoextra" %in% rownames(installed.packages()) == FALSE) {install.packages("factoextra",dependencies=TRUE)};library(factoextra)
 # Surrogate Residuals for Ordinal and General Regression Models
 
-if("sure" %in% rownames(installed.packages()) == FALSE) {install.packages("sure")};library(sure)
+if("sure" %in% rownames(installed.packages()) == FALSE) {install.packages("sure",dependencies=TRUE)};library(sure)
 
 
 # Fonctions diverses pour les graphiques "Grid"(grilles)
-if("gridExtra" %in% rownames(installed.packages()) == FALSE) {install.packages("gridExtra")};library(gridExtra)
+if("gridExtra" %in% rownames(installed.packages()) == FALSE) {install.packages("gridExtra",dependencies=TRUE)};library(gridExtra)
 
 
 
 
+# install.packages("package_name", repos=c("http://rstudio.org/_packages",
+                                         # "http://cran.rstudio.com",dependencies=TRUE))
 
 # CONFLIT : confilt de select() entre les deux librairies
 require(MASS)
